a620bcd58e6f3b2ea2d33f2b1de2329116e92f23
Victor Bologna <victorbologna@hotmail.com>
2026-01-11 18:37:42 -0300

    Added Entity Scan package.

--- mainApp/src/main/java/com/bologna/devices/DevicesApplication.java
+++ mainApp/src/main/java/com/bologna/devices/DevicesApplication.java
@@ -2,8 +2,10 @@ package com.bologna.devices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.bologna.devices")
+@EntityScan(basePackages = "com.bologna.devices.persistence")
 public class DevicesApplication {
 
 	public static void main(String[] args) {
